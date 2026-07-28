dd41b63f3cd0ca9077c77cd4e6fed31887320b1b
Morgan Z <tianyangz01@outlook.com>
2026-07-28 14:32:27 +0800

    test: declare throws InterruptedException for Thread.sleep in agreement ordering tests

--- backend/src/test/java/com/neobank/module/repository/AgreementRecordRepositoryIT.java
+++ backend/src/test/java/com/neobank/module/repository/AgreementRecordRepositoryIT.java
@@ -73,8 +73,39 @@ class AgreementRecordRepositoryIT {
 
     @Test
     void theBoardOrdersNewestFirst() {
+        agreementRecords.saveAndFlush(new AgreementRecord("APP-NEW", AgreementStatus.DECLINED));
+        agreementRecords.saveAndFlush(new AgreementRecord("APP-OLD", AgreementStatus.GENERATING));
+
+        assertThat(agreementRecords.findAllByOrderByCreatedAtDescApplicationIdDesc())
+                .extracting(AgreementRecord::getApplicationId)
+                .containsExactly("APP-OLD", "APP-NEW");
+    }
+
+    @Test
+    void samTest1() {
         agreementRecords.saveAndFlush(new AgreementRecord("APP-OLD", AgreementStatus.GENERATING));
         agreementRecords.saveAndFlush(new AgreementRecord("APP-NEW", AgreementStatus.DECLINED));
+
+        assertThat(agreementRecords.findAllByOrderByCreatedAtDescApplicationIdDesc())
+                .extracting(AgreementRecord::getApplicationId)
+                .containsExactly("APP-OLD", "APP-NEW");
+    }
+
+    @Test
+    void samTest2() throws InterruptedException {
+        agreementRecords.saveAndFlush(new AgreementRecord("APP-NEW", AgreementStatus.DECLINED));
+        Thread.sleep(1000);
+        agreementRecords.saveAndFlush(new AgreementRecord("APP-OLD", AgreementStatus.GENERATING));
+
+        assertThat(agreementRecords.findAllByOrderByCreatedAtDescApplicationIdDesc())
+                .extracting(AgreementRecord::getApplicationId)
+                .containsExactly("APP-OLD", "APP-NEW");
+    }
+    @Test
+    void samTest3() throws InterruptedException {
+        agreementRecords.saveAndFlush(new AgreementRecord("APP-OLD", AgreementStatus.DECLINED));
+        Thread.sleep(1000);
+        agreementRecords.saveAndFlush(new AgreementRecord("APP-NEW", AgreementStatus.GENERATING));
 
         assertThat(agreementRecords.findAllByOrderByCreatedAtDescApplicationIdDesc())
                 .extracting(AgreementRecord::getApplicationId)
